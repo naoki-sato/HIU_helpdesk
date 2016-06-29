71fada66e0980d17c82cc2cb8baec5b99707296b
naoki <aither.coe@gmail.com>
2016-06-29 23:01:26 +0900

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
             $table->increments("id");
             $table->string("user_name");
             $table->integer("user_cd")->unique();
-            $table->integer("phone_no")->nullable();
+            $table->string("phone_no")->nullable();
             $table->string("idm")->nullable();
             $table->timestamps();
         });
